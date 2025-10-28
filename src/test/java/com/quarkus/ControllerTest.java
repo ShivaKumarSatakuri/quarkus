ab11d8ba95bd061181815803b6343dbfc89d634c
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-10-28 16:57:14 +0530

    Added test cases for path variable API

--- src/test/java/com/quarkus/ControllerTest.java
+++ src/test/java/com/quarkus/ControllerTest.java
@@ -1,20 +1,46 @@
 package com.quarkus;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
 public class ControllerTest {
 
     @Test
-    public void test(){
-        RestAssured.given()
+    public void test() {
+        given()
                 .when().get("/hello")
                 .then()
                 .statusCode(200)
                 .body(is("Hello world!"));
+    }
+
+    @Test
+    public void testTempEndpoint_whenTestIdIs1_returnsShiva() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/test/1")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("name", equalTo("Shiva"));
+    }
+
+    @Test
+    public void testTempEndpoint_whenTestIdIsNot1_returnsSai() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/test/5")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(2))
+                .body("name", equalTo("Sai"));
     }
 }
